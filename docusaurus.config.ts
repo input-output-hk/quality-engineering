c653c37e749b106a359b62cbc694eedbc3856a6a
Filip Stoyanov <filip.stoyanov@iohk.io>
2024-10-24 16:29:25 +0100

    filip(chore): add social card and favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,8 +58,7 @@ const config: Config = {
 	],
 
 	themeConfig: {
-		// TODO: Replace with your project's social card
-		// image: 'img/docusaurus-social-card.jpg',
+		image: 'img/social-card.png',
 		colorMode: {
 			disableSwitch: true,
 		},
